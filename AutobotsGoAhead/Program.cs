81683ab66f53409281f8c6a6fca01f4a5c3c5678
YourSneakyLiar <tvoyhleb@gmail.com>
2024-01-24 12:32:58 +0300

    Stikers

--- AutobotsGoAhead/Program.cs
+++ AutobotsGoAhead/Program.cs
@@ -105,7 +105,45 @@ namespace AutobotsGoAhead
 
 
 
-           
+            List<string> imageUrls = new List<string>
+            {
+                "https://github.com/YourSneakyLiar/BoopBot/blob/main/cb9a4359-279d-4153-8f5a-35c29a38073a.jpg?raw=true",
+                "https://github.com/YourSneakyLiar/BoopBot/blob/main/b5d623cf-26e7-4197-af61-5835fec35573.jpg?raw=true",
+                "https://github.com/YourSneakyLiar/BoopBot/blob/main/b42d362d-d464-4e81-8135-39b99f005367.jpg?raw=true",
+                "https://github.com/YourSneakyLiar/BoopBot/blob/main/8463fbb2-cb9e-49ff-b5ff-472b03080698.jpg?raw=true",
+                "https://github.com/YourSneakyLiar/BoopBot/blob/main/7f396adc-b356-4bf6-b179-5afc53b1b9b6.jpg?raw=true",
+                "https://github.com/YourSneakyLiar/BoopBot/blob/main/3daca018-b54d-4bc4-8195-d444276b04ca.jpg?raw=true",
+                "https://github.com/YourSneakyLiar/BoopBot/blob/main/2fbc98e2-8253-4e95-b2f5-b72706021091.jpg?raw=true",
+                // добавьте URL-адреса других изображений...
+            };
+
+            if (message.Text == "Мем")
+            {
+                Random random = new Random();
+                int index = random.Next(imageUrls.Count); // Выберите случайное изображение из списка
+
+                await botClient.SendPhotoAsync(
+                    chatId: chatId,
+                    photo: InputFile.FromUri(imageUrls[index]), // Отправьте случайное изображение в ответ
+                    cancellationToken: cancellationToken
+                );
+            }
+
+            if (message.Text == "Стикер")
+            {
+                var stickerSet = await botClient.GetStickerSetAsync("BoopBot");
+                var stickers = stickerSet.Stickers;
+
+                Random random = new Random();
+                int index = random.Next(stickers.Length); // Выберите случайный стикер из списка
+
+                await botClient.SendStickerAsync(
+                    chatId: chatId,
+                    sticker: InputFile.FromUri(stickers[index].FileId), // Отправьте случайный стикер в ответ
+                    cancellationToken: cancellationToken
+                );
+            }
+
 
         }
 
